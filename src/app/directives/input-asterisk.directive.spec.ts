ebdba32fc8e1cb23e7a6d21203e0f6535a7d3406
Kamil Konopka <kamil.konopka@thomsonreuters.com>
2018-07-18 07:39:40 +0200

    removed unnecessary directive

--- src/app/directives/input-asterisk.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InputAsteriskDirective } from './input-asterisk.directive';
-
-describe('InputAsteriskDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InputAsteriskDirective();
-    expect(directive).toBeTruthy();
-  });
-});
